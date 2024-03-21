9d88789e4025ee224e20cd75af814c96aa24c73f
Mateus Henrique Ferraz <ferraz_dev@proton.me>
2024-03-21 13:04:41 -0300

    add input

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+use num_cpus;
+use std::io::{stdin, stdout, Write};
 use std::net::SocketAddr;
-use std::sync::{Arc, atomic::{AtomicUsize, Ordering}};
+use std::sync::{
+    atomic::{AtomicUsize, Ordering},
+    Arc,
+};
 use tokio::net::UdpSocket;
 use tokio::sync::Mutex;
-use std::io::{stdin,stdout,Write};
-use tokio::time::{self, Duration, Instant};
-use tokio::runtime::Builder;
-use num_cpus; 
+use tokio::time::{Duration, Instant};
 
 #[derive(Debug)]
 struct Stats {
@@ -29,8 +31,8 @@ impl Stats {
         self.successful_requests += 1;
     }
 
-    fn success_rate(&self, duration: Duration) -> f64 {
-        self.successful_requests as f64 
+    fn success_rate(&self) -> f64 {
+        self.successful_requests as f64
     }
 }
 
@@ -42,7 +44,7 @@ async fn make_udp_requests(
     counter: Arc<AtomicUsize>,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let socket = UdpSocket::bind("0.0.0.0:0").await?;
-    let mut interval = time::interval(Duration::from_secs(1));
+    // let interval = time::interval(Duration::from_secs(1));
     let start_time = Instant::now();
     let mut last_tick_time = start_time;
 
@@ -51,7 +53,7 @@ async fn make_udp_requests(
             let mut stats_lock = stats.lock().await;
             stats_lock.increment_total();
         }
-        
+
         socket.send_to(message, &server_address).await?;
 
         {
@@ -62,12 +64,8 @@ async fn make_udp_requests(
         counter.fetch_add(1, Ordering::SeqCst);
 
         if Instant::now() - last_tick_time >= Duration::from_secs(1) {
-            let elapsed_time = start_time.elapsed();
-            let mut stats_lock = stats.lock().await;
-            println!(
-                "Success rate: {:} reqs",
-                stats_lock.success_rate(elapsed_time)
-            );
+            let stats_lock = stats.lock().await;
+            println!("Success rate: {:} reqs", stats_lock.success_rate());
             println!("Total requests: {}", counter.load(Ordering::SeqCst));
             last_tick_time = Instant::now();
         }
@@ -83,60 +81,71 @@ fn validate_user_input(ip: &str) -> bool {
         return false;
     }
 
-    for octet in octets {
-        match octet.parse::<u8>() {
-            Ok(num) => {
-                if num > 255 {
-                    return false;
-                }
-            }
-            Err(_) => return false,
-        }
-    }
+    // for octet in octets {
+    //     match octet.parse::<u8>() {
+    //         Ok(num) => {
+    //             if num > 255 {
+    //                 return false;
+    //             }
+    //         }
+    //         Err(_) => return false,
+    //     }
+    // }
 
     true
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut s = String::new();
 
-    let mut s=String::new();
+    loop {
+        print!("Enter the IP address and port in this format: (IP:PORT): ");
+        let _ = stdout().flush();
 
-    print!("Enter the ip addr and port in this format: IP:PORT: ");
+        stdin().read_line(&mut s)?;
 
-    let _=stdout().flush();
+        if let Some('\n') = s.chars().next_back() {
+            s.pop();
+        }
+        if let Some('\r') = s.chars().next_back() {
+            s.pop();
+        }
 
-    stdin().read_line(&mut s).expect("Did not enter a correct ip address, if the error persists contact the developer or make a pull request :)");
-
-    if let Some('\n')=s.chars().next_back() {
-        s.pop();
+        if validate_user_input(&s) {
+            break;
+        } else {
+            println!("Invalid input. Please try again.");
+        }
     }
-    if let Some('\r')=s.chars().next_back() {
-        s.pop();
-    }
 
-    if !validate_user_input(s) {
-        Ok(())
-    }
+    println!("{}", s);
 
     let server_address: SocketAddr = s.parse()?;
-    let message = vec![0; 1024]; 
-    let num_requests_per_instance = 62500000; 
-    let num_instances = num_cpus::get(); 
+    let message = vec![0; 1024];
+    let num_requests_per_instance = 62500000;
+    let num_instances = num_cpus::get();
     let stats = Arc::new(Mutex::new(Stats::new()));
     let counter = Arc::new(AtomicUsize::new(0));
 
     let mut handles = vec![];
 
-
     for _ in 0..num_instances {
         let server_address = server_address.clone();
-        let message = message.clone(); 
+        let message = message.clone();
         let stats = stats.clone();
         let counter = counter.clone();
 
         let handle = tokio::spawn(async move {
-            if let Err(err) = make_udp_requests(server_address, &message, num_requests_per_instance, stats, counter).await {
+            if let Err(err) = make_udp_requests(
+                server_address,
+                &message,
+                num_requests_per_instance,
+                stats,
+                counter,
+            )
+            .await
+            {
                 eprintln!("Server returned an error: {}", err);
             }
         });
